b1e33d2e656c0b720df697227aa130be56b13d6a
unknown <rhra.rodrigo@gmail.com>
2022-04-27 10:50:54 -0300

    Prmeiro commit

---
+++ src/main/java/com/andradecorp/springbatchtraining/SpringbatchtrainingApplication.java
@@ -0,0 +1,13 @@
+package com.andradecorp.springbatchtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchtrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchtrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andradecorp/springbatchtraining/SpringbatchtrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andradecorp.springbatchtraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchtrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
