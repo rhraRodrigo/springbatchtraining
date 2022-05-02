4fbbcfbd6ed94b86adb89fb84378ce09df5ae155
unknown <rhra.rodrigo@gmail.com>
2022-05-02 11:49:24 -0300

    Aumentando segurança das credenciais de banco de dados e configurando o banco para pegar as credenciais de um file system

---
+++ src/main/java/com/andradecorp/springbatchtraining/config/PropsConfig.java
@@ -0,0 +1,20 @@
+package com.andradecorp.springbatchtraining.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
+
+@Configuration
+public class PropsConfig {
+	
+	
+	@Bean
+	public PropertySourcesPlaceholderConfigurer config() {
+		
+		PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+		configurer.setLocation(new FileSystemResource(System.getenv("DB_MYSQL")));
+		return configurer;
+				
+	}
+}
